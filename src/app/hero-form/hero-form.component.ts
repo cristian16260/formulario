fefd27589234fc618797ac7413605f290d85c619
cmr26 <2017110743@udh.edu.pe>
2023-02-03 19:58:56 -0500

    formulario

---
+++ src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent {
+
+poderes = ['Realmente inteligente', 'Súper flexible',
+              'Super caliente', 'Cambiador de clima']
+
+model = new Hero(18,'Dr. IQ', this.poderes[0], 'Megamente');
+
+submitted = false;
+
+onSubmit() {this.submitted = true; }  
+
+newHero(){
+  this.model = new Hero(42,'','');
+}
+
+}
+
+
+
+
